a612e292aca35ab9f2ff661319c455f25b870f83
yumazu256 <yumazu.fuwafuwa256@gmail.com>
2024-08-27 17:53:16 +0900

    Add Animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,57 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation:{
+				"flow-in-bottom-1":"flow-in-bottom-1 1s ease both",
+				"flow-in-bottom-2":"flow-in-bottom-2 2s ease both",
+			},
+			keyframes: {
+				"flow-in-bottom-1": {
+					"0%": {
+						translate: "0px 14px",
+						opacity: "0",
+					},
+					to: {
+						translate: "0px 0px",
+						opacity: "1",
+					},
+				},
+				"flow-in-bottom-2": {
+					"0%": {
+						translate: "0px 14px",
+						opacity: "0",
+					},
+					"30%": {
+						translate: "0px 14px",
+						opacity: "0",
+					},
+					to: {
+						translate: "0px 0px",
+						opacity: "1",
+					},
+				},
+				"BG-ANIM": {
+					"0%": {
+						opacity: "0.65",
+					},
+					"50%": {
+						opacity: "0.8",
+					},
+					to: {
+						opacity: "0.65",
+					},
+				},
+				"BG-ANIM-OUT": {
+					"0%": {
+						opacity: "0",
+					},
+					to: {
+						opacity: "1",
+					},
+				},
+			}
+		},
 	},
 	plugins: [],
 }
